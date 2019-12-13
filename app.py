a37a873db161006850fd01ef6372abd44dfc5895
kreatinbiceps <drogoxx@protonamil.com>
2019-12-13 16:15:42 +0100

    modified:   app.py 	modified:   link.db 	new file:   static/css/main.css 	new file:   static/css/sheltie.jpg 	new file:   templates/about.html 	new file:   templates/contact.html 	modified:   templates/index.html 	modified:   templates/url.html

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 DATABASE = "./link.db"
 
 
+# If the Database does not exist, create one and just insert something
 if not os.path.exists(DATABASE):
 	con = lite.connect(DATABASE)
 	cursor = con.cursor()
@@ -21,29 +22,27 @@ if not os.path.exists(DATABASE):
 	con.commit()
 	con.close()
 
-
+# Writing the generated 4-char string and the original URL to the database. Calling "nginx-from-db.py which creates the nginx files from the database
 def writeToDB(longurl, shorturl):
 	con = lite.connect(DATABASE)
 	cursor = con.cursor()
 	cursor.execute("INSERT INTO url (ID, ORIGURL, SHORTURL) VALUES(?,?,?)", (None, longurl, shorturl) )
-
-#	data_touple = (longurl, shorturl)
-#	cursor.execute(sqlite_insert_q, data_touple)
 
 	con.commit()
 	con.close()
 
 	subprocess.call(["sudo", "python3", "nginx-from-db.py"])
 
+# Creating the random string
 def randomString(stringLength=4):
 	letters = string.ascii_lowercase
 	return ''.join(random.choice(letters) for i in range (stringLength))
 
 
-
-def createNginx(url, shorturl):
-	nginx = '\nlocation /'+ shorturl +' {return 301 ' + url + ';}'
-	return nginx
+# Creates the nginx string needed but I changed this to read from the DB instead in "nginx-from-db.py"
+#def createNginx(url, shorturl):
+#	nginx = '\nlocation /'+ shorturl +' {return 301 ' + url + ';}'
+#	return nginx
 
 
 
@@ -52,33 +51,43 @@ def index():
 	return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+
 
 @app.route('/url',methods = ['POST', 'GET'])
 def index2():
-    #connect to the database
 
-	url = request.args.get('url')
-	longurl = url
-	pattern = re.compile(r'(http|ftp|https)://([\w-]+(?:(?:.[\w-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?', re.IGNORECASE)
-	result = re.search(pattern, url)
-	if result:
-		shorturl = randomString()
-		nginx = createNginx(url, shorturl)
+        url = request.args.get('url')
+        longurl = url
+        pattern = re.compile(r'(http|ftp|https)://([\w-]+(?:(?:.[\w-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?', re.IGNORECASE)
+        result = re.search(pattern, url)
+        if result:
+                shorturl = randomString()
+               # nginx = createNginx(url, shorturl)
 
-		writeToDB(longurl, shorturl)
+                writeToDB(longurl, shorturl)
 
-		con = lite.connect(DATABASE)
-		cursor = con.cursor()
-		cursor.execute("SELECT * FROM url;")
-		urldata = cursor.fetchall()
+                con = lite.connect(DATABASE)
+                cursor = con.cursor()
+                cursor.execute("SELECT * FROM url;")
+                urldata = cursor.fetchall()
 
-		context = {'url': url, 'shorturl': shorturl}
-		return render_template('url.html', **context, urldata=urldata)
+                context = {'url': url, 'shorturl': shorturl}
+                return render_template('url.html', **context, urldata=urldata)
 
-	else:
-		wrongurl = 'The URL provided was wrong.'
-		context = {'wrongurl': wrongurl}
-		return render_template('index.html', **context)
+        else:
+                wrongurl = 'The URL provided was wrong.'
+                context = {'wrongurl': wrongurl}
+                return render_template('index.html', **context)
 
 
 
